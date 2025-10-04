dfb3b8b6947709558d67a3d2ce82ff3c659a8700
Felipe Borges <lipelsborges@gmail.com>
2025-10-04 17:12:49 -0300

    atualizaçoes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 
+const scoreEl = document.getElementById('score')
+const highscoreEl = document.getElementById('highscore')
+
+let score = 0
+
+let highscore = Number(localStorage.getItem('marioHighscore') || 0)
+highscoreEl.textContent = highscore
+
+let lastPipePosition = pipe.offsetLeft 
+
 const jump = () => {
     mario.classList.add('jump');
 
@@ -27,8 +37,21 @@ const loop = setInterval(() =>{
         mario.style.marginLeft = '50px' 
 
         clearInterval(loop)
+        return
     }
+    if (lastPipePosition > 0 && pipePosition <= 0) {
+    score += 1
+    scoreEl.textContent = score
 
+    if (score > highscore) {
+      highscore = score
+      highscoreEl.textContent = highscore
+      localStorage.setItem('marioHighscore', String(highscore))
+    }
+  }
+
+  
+  lastPipePosition = pipePosition
 
 }, 10);
 
